aed0eeb1014d6bb422b1e6e5960db4a5ef92b381
Boris Resnick <boris.resnick@gmail.com>
2024-07-10 07:23:28 +0300

    Added minimal setup.py - continue

--- setup.py
+++ setup.py
@@ -3,5 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='passwordmeter',
     version='2.0.0',
-    packages=find_packages()
+    packages=find_packages(),
+    install_requires=[],
+    platforms=['any']
 )
